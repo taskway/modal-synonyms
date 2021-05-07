843990939c582003dd244b95bc147570bf17e49d
Храмов Алексей Игоревич <out_khramov.ai@pecom.ru>
2021-05-07 11:27:38 +0300

    Fixed

--- src/features/Synonyms/reducer.ts
+++ src/features/Synonyms/reducer.ts
@@ -7,25 +7,25 @@ export const SynonymsReducer = (state = initialState, action: ActionTypes): type
     case 'ADD_SYNONYM':
       return [...state, { id: state.length + 1, title: action.title, edit: false }]
     case 'UPDATE_SYNONYM': {
-      const updateSynonymIndex = state.findIndex((synonym) => synonym.id === action.payload.id)
-      const updateSynonyms = [...state]
-      updateSynonyms[updateSynonymIndex] = {
-        ...updateSynonyms[updateSynonymIndex],
+      const updatedSynonymIndex = state.findIndex((synonym) => synonym.id === action.payload.id)
+      const updatedSynonyms = [...state]
+      updatedSynonyms[updatedSynonymIndex] = {
+        ...updatedSynonyms[updatedSynonymIndex],
         title: action.payload.title,
         edit: false
       }
-      return updateSynonyms
+      return updatedSynonyms
     }
     case 'REMOVE_SYNONYM':
       return state.filter((i) => i.id !== action.id)
     case 'SET_EDIT_MODE_SYNONYM': {
-      const updateSynonymIndex = state.findIndex((synonym) => synonym.id === action.id)
-      const updateSynonyms = [...state]
-      updateSynonyms[updateSynonymIndex] = {
-        ...updateSynonyms[updateSynonymIndex],
-        edit: !updateSynonyms[updateSynonymIndex].edit
+      const updatedSynonymIndex = state.findIndex((synonym) => synonym.id === action.id)
+      const updatedSynonyms = [...state]
+      updatedSynonyms[updatedSynonymIndex] = {
+        ...updatedSynonyms[updatedSynonymIndex],
+        edit: !updatedSynonyms[updatedSynonymIndex].edit
       }
-      return updateSynonyms
+      return updatedSynonyms
     }
     case 'CLEAR_SYNONYMS':
       return []
